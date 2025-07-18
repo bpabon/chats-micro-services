08bff86726cb56e2571ebc7e716ab442a5f6017a
unknown <bpabon1@udi.edu.co>
2025-07-18 18:44:32 -0500

    Ajustes clases

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}',
+    './src/**/*.css', // si usas CSS directamente
+  ],
+  theme: {
+    extend: {},
+  },
+  safelist: ['space-y-6'],
+  plugins: [],
+}
