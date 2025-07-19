b9fffaf5ed6e80aa530b34c00542e8a8e53471ee
unknown <bpabon1@udi.edu.co>
2025-07-19 10:41:12 -0500

    Ajustes tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
-// tailwind.config.js
+const forms = require('@tailwindcss/forms');
+const typography = require('@tailwindcss/typography');
+const aspectRatio = require('@tailwindcss/aspect-ratio');
+const scrollbar = require('tailwind-scrollbar');
+
 module.exports = {
   content: [
     './src/**/*.{html,ts}',
     './src/**/*.css', // si usas CSS directamente
   ],
+  safelist: ['space-y-6', 'ease-in', 'duration-100', 'transition'],
   theme: {
     extend: {},
   },
-  safelist: ['space-y-6'],
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-    require('tailwind-scrollbar'),
-  ],
+  plugins: [forms, typography, aspectRatio, scrollbar],
+
 
 }
