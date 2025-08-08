e4b7ceb7e94f5737d6732fc8cf9a49f993cff635
Branm Aldair Pabon Villamizar <branm.pabon@grupoemi.com>
2025-08-08 15:46:22 -0500

    Ajuste vista de chat

--- src/app/modules/dashboard/components/chats/chats.component.ts
+++ src/app/modules/dashboard/components/chats/chats.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactsComponent } from './contacts/contacts.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-chats',
-  imports: [],
+  imports: [ContactsComponent, MessagesComponent],
   standalone: true,
   templateUrl: './chats.component.html',
   styleUrl: './chats.component.css'
